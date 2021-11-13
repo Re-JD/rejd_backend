546d756535963788c9c29cfa70995147f3848dbc
chiyoung0011 <jcy0011@naver.com>
2021-11-14 06:53:52 +0900

    parameter mismatch - fixed, bug allowing users to login without auth - fixed

--- backend_developer_test/login/serializers.py
+++ backend_developer_test/login/serializers.py
@@ -1,4 +1,4 @@
-from .models import User
+from .models import User, UserManager
 from rest_framework import serializers
 from django.contrib.auth import *
 from rest_framework_jwt.settings import api_settings
@@ -35,25 +35,35 @@ class UserLoginSerializer(serializers.Serializer):
         }
 
 class RegisterSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(max_length=255)
     password = serializers.CharField(
         max_length=255, min_length=8, write_only=True
     )
+    password2 = serializers.CharField(
+        max_length=255, min_length=8, write_only=True
+    )
+    email = serializers.EmailField(
+        max_length=255,
+    )
+    
 
     class Meta:
         model = User
-        fields = ['email', 'username', 'password']
+        fields = ['email', 'username', 'password', 'password2']
 
     def validate(self, attrs):
         email = attrs.get('email', '')
         username = attrs.get('username', '')
+        if attrs.get('password', '') == attrs.get('password2', ''):
+            password = attrs.get('password', '')
         return attrs
     
     def create(self, validated_data):
+        _ = validated_data.pop('password2', 0)
         return User.objects.create_user(**validated_data)
 
 class EmailVerificationSerializer(serializers.ModelSerializer):
-    token = serializers.CharField(max_length=555)
-    
+    token = serializers.CharField(max_length=5555)
     class Meta:
         model = User
         fields = ['token']
